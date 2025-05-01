f4fcb55a08a5781f24f05bf7d64a28f45aec7d15
itsmearion <bilqiss0893@gmail.com>
2025-05-02 01:03:33 +0700

    Update g.py

--- g.py
+++ g.py
@@ -1,10 +1,20 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-api_id = int(input("Masukkan API ID: "))
-api_hash = input("Masukkan API Hash: ")
+print("=== Generate String Session ===")
+api_id = int(input("API ID: "))
+api_hash = input("API Hash: ")
 
-with TelegramClient(StringSession(), api_id, api_hash) as client:
+with TelegramClient(
+    StringSession(),
+    api_id,
+    api_hash,
+    device_model="TermuxAndroid",
+    system_version="13",
+    app_version="10.0",
+    lang_code="en",
+    system_lang_code="en"
+) as client:
     print("Login ke Telegram...")
     session_string = client.session.save()
     print("\n✅ String session kamu:\n")
